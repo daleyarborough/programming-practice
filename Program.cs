2f6769bbc85236ea56148d3c5323213b4d11b796
Dale.Yarborough <dale.yarborough@wholefoods.com>
2021-06-07 13:42:33 -0500

    adding new dotnet console app proj

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace programming_practice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
